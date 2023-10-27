4242280a7603f67d2bbca4d99f54936046ba00da
Emre Deniz <emre.deniz@outlook.com>
2023-10-27 00:26:28 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team3_MAPD714_Project-Milestone2
+//
+//  Created by Emre Deniz on 2023-10-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

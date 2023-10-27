3664163daedf81fbb0d2168645c575c7ed543d7d
Emre Deniz <emre.deniz@outlook.com>
2023-10-27 01:45:10 -0400

    Search and Cruise view controllers created

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // Login button
+    @IBAction func loginButtonAction (_ sender: Any) {
+        let control = storyboard?.instantiateViewController(withIdentifier: "login") as!
+        LoginViewController
+        
+        present(control, animated: true)
+    }
+    
+    // Register button
+    @IBAction func registerButtonAction (_ sender: Any) {
+        let control = storyboard?.instantiateViewController(withIdentifier: "register") as! RegisterViewController
+        
+        present(control, animated: true)
+    }
+    
+    // Search button
+    @IBAction func searchButtonAction (_ sender: Any) {
+        let control = storyboard?.instantiateViewController(withIdentifier: "search") as! SearchViewController
+        
+        present(control, animated: true)
+    }
+    
 
 
 }

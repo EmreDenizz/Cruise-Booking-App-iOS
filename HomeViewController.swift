c84bc02e30d26352a469df6fcbbc384ccdae2725
Nkemjika Obi <nkemjikaobi@gmail.com>
2023-10-29 14:18:57 -0400

    Search Screen, Home Screen and Cruise Detail Screen with respective controllers

---
+++ HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Team3_MAPD714_Project-Milestone2
+//
+//  Created by Obi Nkemjika Derick on 2023-10-28.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

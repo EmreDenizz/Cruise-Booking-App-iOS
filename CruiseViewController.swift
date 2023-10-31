ccb3cbf171dca5f1a50fb6a64ae261e8180660aa
Muindo N. Gituku <ngeenegituku@gmail.com>
2023-10-31 08:51:58 -0400

    Final Submission

--- CruiseViewController.swift
+++ CruiseViewController.swift
@@ -3,6 +3,13 @@
 //  Team3_MAPD714_Project-Milestone2
 //
 //  Created by Emre Deniz on 2023-10-27.
+//  @author Emre Deniz (301371047)
+//  @author Nkemjika Obi (301275091)
+//  @author Muindo Gituku (301372521)
+//  @date 2023-10-30
+//  @description iOS Project - Milestone 2
+
+// Github Repo: https://github.com/EmreDenizz/Cruise-Booking-App-iOS
 //
 
 import UIKit
--- HomeViewController.swift
+++ HomeViewController.swift
@@ -3,26 +3,42 @@
 //  Team3_MAPD714_Project-Milestone2
 //
 //  Created by Obi Nkemjika Derick on 2023-10-28.
+//  @author Emre Deniz (301371047)
+//  @author Nkemjika Obi (301275091)
+//  @author Muindo Gituku (301372521)
+//  @date 2023-10-30
+//  @description iOS Project - Milestone 2
+
+// Github Repo: https://github.com/EmreDenizz/Cruise-Booking-App-iOS
 //
 
 import UIKit
 
 class HomeViewController: UIViewController {
     
+    
     //Reference to the cruise image
-    @IBOutlet weak var cruiseImageView: UIImageView!
-
+    @IBOutlet weak var cruiseImageView1: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // create tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cruiseDetailAction))
+        let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(cruiseDetailAction1))
+        
 
         // add gesture to the image view;
-        cruiseImageView.addGestureRecognizer(tapGesture)
+        cruiseImageView1.addGestureRecognizer(tapGesture1)
+        
+        
         
     }
     
+    @IBAction func openCruise1(_ sender: Any) {
+        //let control = storyboard?.instantiateViewController(withIdentifier: "cruise") as! CruiseViewController
+        //present(control, animated: true)
+        print("Testing")
+    }
     //Search input action - This allows the user move to the search screen they click the input field
     @IBAction func searchButtonAction (_ sender: Any) {
         let control = storyboard?.instantiateViewController(withIdentifier: "search") as! SearchViewController
@@ -31,11 +47,11 @@ class HomeViewController: UIViewController {
     }
     
     //Cruise detail action
-    @objc func cruiseDetailAction (gesture: UIGestureRecognizer) {
+    @objc func cruiseDetailAction1 (gesture: UIGestureRecognizer) {
         let control = storyboard?.instantiateViewController(withIdentifier: "cruise") as! CruiseViewController
-        
         present(control, animated: true)
     }
+    
     
 
     /*

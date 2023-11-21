27b814414407f2e00ff48ead70ccdf4b1a4b7cbf
Emre Deniz <emre.deniz@outlook.com>
2023-11-20 23:55:33 -0500

    Create SQLIte DB & Cruise Table & Cruise Model Methods

--- Cruise.swift
+++ Cruise.swift
@@ -2,7 +2,24 @@
 //  Cruise.swift
 //  Team3_MAPD714_Project-Milestone2
 //
-//  Created by Emre Deniz on 2023-11-20.
+//  @author Emre Deniz (301371047)
+//  @author Nkemjika Obi (301275091)
+//  @author Muindo Gituku (301372521)
+//  @date 2023-11-27
+//  @description iOS Project - Milestone 4
+//  Github Repo: https://github.com/EmreDenizz/Cruise-Booking-App-iOS
 //
 
 import Foundation
+
+class Cruise{
+    var id: Int
+    var name: String
+    var price: Int
+    
+    init(id: Int, name: String, price: Int) {
+        self.id = id
+        self.name = name
+        self.price = price
+    }
+}
--- CruiseDBManager.swift
+++ CruiseDBManager.swift
@@ -1,8 +1,132 @@
 //
 //  CruiseDBManager.swift
-//  Team3_MAPD714_Project-Milestone2
+//  Team3_MAPD714_Project-Milestone3
 //
-//  Created by Emre Deniz on 2023-11-20.
+//  @author Emre Deniz (301371047)
+//  @author Nkemjika Obi (301275091)
+//  @author Muindo Gituku (301372521)
+//  @date 2023-11-27
+//  @description iOS Project - Milestone 4
+//  Github Repo: https://github.com/EmreDenizz/Cruise-Booking-App-iOS
 //
 
 import Foundation
+import SQLite3
+
+class CruiseDBManager{
+    init(){
+        db = openDatabase()
+        createCruiseTable()
+        createUserTable()
+    }
+    
+    let dataPath: String = "CruiseDB"
+    var db: OpaquePointer?
+    
+    // Create DB
+    func openDatabase()->OpaquePointer?{
+        var filePath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent(dataPath)
+        
+        var db: OpaquePointer? = nil
+        if sqlite3_open(filePath.path, &db) != SQLITE_OK{
+            debugPrint("Cannot open database")
+            return nil
+        }
+        else{
+            print("database successfully created.")
+            return db
+        }
+    }
+    
+    // Create users table
+    func createUserTable(){
+        
+    }
+    
+    // Create cruise table
+    func createCruiseTable(){
+        var createTableString = "CREATE TABLE IF NOT EXISTS Cruise (id INTEGER PRIMARY KEY, name TEXT, price INTEGER);"
+        
+        var createTableStatement: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(db, createTableString, -1, &createTableStatement, nil) == SQLITE_OK{
+            if sqlite3_step(createTableStatement) == SQLITE_DONE{
+                print("cruise tabel created successfully")
+            }
+            else{
+                print("cruise table failed")
+            }
+        }
+        else{
+            print("failed create statement")
+        }
+        
+        sqlite3_finalize(createTableStatement)
+    }
+    
+    // Insert into cruise table
+    func insertCruise(id:Int, name:String, price:Int){
+        let cruises = getCruises()
+        for cruise in cruises{
+            if cruise.id == id{
+                return
+            }
+        }
+        
+        let insertStatementString = "INSERT INTO Cruise (id, name, price) VALUES (?, ?, ?);"
+        var insertStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
+            sqlite3_bind_int(insertStatement, 1, Int32(id))
+            sqlite3_bind_text(insertStatement, 2, (name as NSString).utf8String, -1, nil)
+            sqlite3_bind_int(insertStatement, 3, Int32(price))
+
+            if sqlite3_step(insertStatement) == SQLITE_DONE {
+                print("A cruise added successfully!")
+            } else {
+                print("Couldn't add any row?")
+            }
+        } else {
+            print("INSERT statement failed to succeed!!!")
+        }
+        sqlite3_finalize(insertStatement)
+    }
+    
+    // Get cruises from Cruise table
+    func getCruises() -> [Cruise] {
+        let queryStatementString = "SELECT * FROM Cruise;"
+        var queryStatement: OpaquePointer? = nil
+        var prods : [Cruise] = []
+        if sqlite3_prepare_v2(db, queryStatementString, -1, &queryStatement, nil) == SQLITE_OK {
+            while sqlite3_step(queryStatement) == SQLITE_ROW {
+                let id = sqlite3_column_int(queryStatement, 0)
+                let name = String(describing: String(cString: sqlite3_column_text(queryStatement, 1)))
+                let price = sqlite3_column_int(queryStatement, 2)
+                prods.append(Cruise(id: Int(id), name: name, price: Int(price)))
+                print("Cruise Details:")
+                print("\(id) | \(name) | \(price)")
+            }
+        } else {
+            print("SELECT statement failed to proceed!!!")
+        }
+        sqlite3_finalize(queryStatement)
+        return prods
+    }
+    
+    // Delete from Cruise table
+    func deleteCruiseByID(id:Int) {
+        let deleteStatementStirng = "DELETE FROM Cruise WHERE id = ?;"
+        var deleteStatement: OpaquePointer? = nil
+        if sqlite3_prepare_v2(db, deleteStatementStirng, -1, &deleteStatement, nil) == SQLITE_OK {
+            sqlite3_bind_int(deleteStatement, 1, Int32(id))
+            if sqlite3_step(deleteStatement) == SQLITE_DONE {
+                print("A cruise deleted successfully!")
+            } else {
+                print("Couldn't delete given cruise")
+            }
+        } else {
+            print("DELETE statement failed to succeed!")
+        }
+        sqlite3_finalize(deleteStatement)
+    }
+    
+}

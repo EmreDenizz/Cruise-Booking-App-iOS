98b041667d2a9dfc912753d73d830bae084f2e28
Emre Deniz <emre.deniz@outlook.com>
2023-11-20 23:53:29 -0500

    UI improvements

---
+++ Cruise.swift
@@ -0,0 +1,8 @@
+//
+//  Cruise.swift
+//  Team3_MAPD714_Project-Milestone2
+//
+//  Created by Emre Deniz on 2023-11-20.
+//
+
+import Foundation
---
+++ CruiseDBManager.swift
@@ -0,0 +1,8 @@
+//
+//  CruiseDBManager.swift
+//  Team3_MAPD714_Project-Milestone2
+//
+//  Created by Emre Deniz on 2023-11-20.
+//
+
+import Foundation
